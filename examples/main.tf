e7e61d3e1f4056eab74aef18a0485bfbb31d1a2b
Rowan Matulis <rowan.matulis@optimatech.io>
2022-12-12 10:01:36 +1100

    init

---
+++ examples/main.tf
@@ -0,0 +1,16 @@
+#
+terraform {
+  required_version = ">= 0.13.1"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.73"
+    }
+  }
+}
+
+
+module "root-login-notification" {
+  source = "../"
+}
