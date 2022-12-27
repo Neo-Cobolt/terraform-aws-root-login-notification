e6940d33b9b128e6252d585c88082347a49745fc
Rowan Matulis <rowan.matulis@optimatech.io>
2022-12-27 14:03:01 +1100

    adding description to vars

--- variables.tf
+++ variables.tf
@@ -1,16 +1,16 @@
 variable "email_display_name" {
   default     = ""
   type        = string
-  description = "value"
+  description = "Display name of the email sent to users. It is best to name this after the account so you can tell accounts apart easily."
 }
 
 variable "email_address_list" {
   type        = list(any)
-  description = "value"
+  description = "List of email addresses to email from sns topic."
 }
 
 variable "notification_type" {
   type        = string
   default     = "email-json"
-  description = "value"
+  description = "The type of notification to deliver, currently setup for email and email-json only."
 }
